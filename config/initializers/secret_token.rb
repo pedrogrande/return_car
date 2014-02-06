16f4c8a099f9682eb246bf48d2f2e0508f875775
Chase Clettenberg <clettenberg@gmail.com>
2014-02-06 19:39:03 +0900

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReturnCar::Application.config.secret_key_base = '805a7c431443548412048e6fdf01e25d8f7f9f2c77400a2a2f236ca8b594efa2670af94123a200eadee17d4fbe76adba35d01d5a5e25a94c0cab108f528f7b41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReturnCar::Application.config.session_store :cookie_store, key: '_return_car_session'
