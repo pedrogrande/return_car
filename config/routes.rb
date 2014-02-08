4600f33dc0561af2bf765cb77077d90a7af41526
Chase Clettenberg <clettenberg@gmail.com>
2014-02-08 17:35:30 +0900

    Add pods index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ReturnCar::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root :to => 'home#index'
 
   resources :pods
 
