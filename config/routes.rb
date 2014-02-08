a50117087148a57246b3357f71c91f341059bda5
Chase Clettenberg <clettenberg@gmail.com>
2014-02-08 17:23:55 +0900

    Add pods resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ ReturnCar::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :pods
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
